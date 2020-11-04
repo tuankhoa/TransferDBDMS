6743786f35c26bebfb201930d3f20f61c1050155
Tuan Khoa <tuankhoa.0013@gmail.com>
2020-11-04 10:43:42 +0700

    initial project

---
+++ app.js
@@ -0,0 +1,9 @@
+// Import
+const express = require('express')
+
+// Call api
+
+// Run server
+const app = express()
+const port = 3000
+app.listen(port, () => console.log(`App listening on port ${port}!`))
